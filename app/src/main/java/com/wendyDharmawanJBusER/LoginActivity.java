d614e1edf739ac70baacba5ffe4888f1b3443071
Wendy <wendydharmawan@ui.ac.id>
2023-11-17 12:01:44 -0500

    CS8

--- app/src/main/java/com/wendyDharmawanJBusER/LoginActivity.java
+++ app/src/main/java/com/wendyDharmawanJBusER/LoginActivity.java
@@ -2,13 +2,59 @@ package com.wendyDharmawanJBusER;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wendyDharmawanJBusER.jbus_android.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private TextView registerNow = null;
+    private Button loginNow = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        registerNow = findViewById(R.id.Register_button);
+        loginNow = findViewById(R.id.login_button);
+        registerNow.setOnClickListener(v -> {
+            moveActivity(this, registerActivity.class);
+            viewToast(this, "Move to register");
+        });
+        loginNow.setOnClickListener(v -> {
+            moveActivity(this, mainActivity.class);
+            viewToast(this, "Move to login");
+        });
+        try {
+            getSupportActionBar();
+        } catch (NullPointerException e) {
+
+        }
+    }
+
+
+    private void moveActivity(Context ctx, Class<?> cls) {
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String message) {
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
     }
 }
+
---
+++ app/src/main/java/com/wendyDharmawanJBusER/registerActivity.java
@@ -0,0 +1,14 @@
+package com.wendyDharmawanJBusER;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.wendyDharmawanJBusER.jbus_android.R;
+
+public class registerActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
