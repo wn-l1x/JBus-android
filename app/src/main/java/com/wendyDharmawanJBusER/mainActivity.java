80cb73c2b173ff968ca16b4b6dcb47c40d293ad2
Wendy <wendydharmawan@ui.ac.id>
2023-11-20 10:38:20 -0500

    PT 08

--- app/src/main/java/com/wendyDharmawanJBusER/mainActivity.java
+++ app/src/main/java/com/wendyDharmawanJBusER/mainActivity.java
@@ -1,44 +1,153 @@
 package com.wendyDharmawanJBusER;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.wendyDharmawanJBusER.jbus_android.R;
+import com.wendyDharmawanJBusER.model.Bus;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class mainActivity extends AppCompatActivity {
+    private androidx.appcompat.widget.AppCompatButton userprofile_button = null;
+    private Button[] btns;
+    private int currentPage = 0;
+    private int pageSize = 12; // kalian dapat bereksperimen dengan field ini
+    private int listSize;
+    private int noOfPages;
+    private List<Bus> listBus = new ArrayList<>();
+    private Button prevButton = null;
+    private Button nextButton = null;
+    private ListView busListView = null;
+    private HorizontalScrollView pageScroll = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BusArrayAdapter numbersArrayAdapter = new BusArrayAdapter(this, Bus.sampleBusList(20));
+        ListView numbersListView = findViewById(R.id.listView);
+        numbersListView.setAdapter(numbersArrayAdapter);
+
+        prevButton = findViewById(R.id.prev_page);
+        nextButton = findViewById(R.id.next_page);
+        pageScroll = findViewById(R.id.page_number_scroll);
+        busListView = findViewById(R.id.listView);
+        // membuat sample list
+        listBus = Bus.sampleBusList(20);
+        listSize = listBus.size();
+        // construct the footer
+        paginationFooter();
+        goToPage(currentPage);
+        // listener untuk button prev dan button
+        prevButton.setOnClickListener(v -> {
+            currentPage = currentPage != 0? currentPage-1 : 0;
+            goToPage(currentPage);
+        });
+        nextButton.setOnClickListener(v -> {
+            currentPage = currentPage != noOfPages -1? currentPage+1 : currentPage;
+            goToPage(currentPage);
+        });
+
     }
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.app_bar_search){
-            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
-        } else if (id == R.id.app_bar_profile) {
-            Toast.makeText(this, "User Profile", Toast.LENGTH_SHORT).show();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if (item.getItemId() == R.id.app_bar_profile) {
             moveActivity(this, AboutMeActivity.class);
-        } else if (id == R.id.app_bar_payment) {
-            Toast.makeText(this, "Payment", Toast.LENGTH_SHORT).show();
+            return true;
         }
-        return true;
+        return super.onOptionsItemSelected(item);
     }
+
     private void moveActivity(Context ctx, Class<?> cls){
         Intent intent = new Intent(ctx, cls);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    private void paginationFooter() {
+        int val = listSize % pageSize;
+        val = val == 0 ? 0:1;
+        noOfPages = listSize / pageSize + val;
+        LinearLayout ll = findViewById(R.id.btn_layout);
+        btns = new Button[noOfPages];
+        if (noOfPages <= 6) {
+            ((FrameLayout.LayoutParams) ll.getLayoutParams()).gravity =
+                    Gravity.CENTER;
+        }
+        for (int i = 0; i < noOfPages; i++) {
+            btns[i]=new Button(this);
+            btns[i].setBackgroundColor(getResources().getColor(android.R.color.transparent));
+            btns[i].setText(""+(i+1));
+            // ganti dengan warna yang kalian mau
+            btns[i].setTextColor(getResources().getColor(R.color.black));
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(100,
+                    100);
+            ll.addView(btns[i], lp);
+            final int j = i;
+            btns[j].setOnClickListener(v -> {
+                currentPage = j;
+                goToPage(j);
+            });
+        }
+    }
+
+    private void goToPage(int index) {
+        for (int i = 0; i< noOfPages; i++) {
+            if (i == index) {
+                btns[index].setBackgroundDrawable(getResources().getDrawable(R.drawable.baseline_circle_24));
+                btns[i].setTextColor(getResources().getColor(android.R.color.white));
+                scrollToItem(btns[index]);
+                viewPaginatedList(listBus, currentPage);
+            } else {
+                btns[i].setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                btns[i].setTextColor(getResources().getColor(android.R.color.black));
+            }
+        }
+    }
+
+    private void scrollToItem(Button item) {
+        int scrollX = item.getLeft() - (pageScroll.getWidth() - item.getWidth()) /
+                2;
+        pageScroll.smoothScrollTo(scrollX, 0);
+    }
+
+    private void viewPaginatedList(List<Bus> listBus, int page) {
+        int startIndex = page * pageSize;
+        int endIndex = Math.min(startIndex + pageSize, listBus.size());
+        List<Bus> paginatedList = listBus.subList(startIndex, endIndex);
+        // Tampilkan paginatedList ke listview
+        // seperti yang sudah kalian lakukan sebelumnya,
+        // menggunakan array adapter.
+        BusArrayAdapter numbersArrayAdapter = new BusArrayAdapter(this, paginatedList);
+
+        // create the instance of the ListView to set the numbersViewAdapter
+        ListView numbersListView = findViewById(R.id.listView);
+
+        // set the numbersViewAdapter for ListView
+        numbersListView.setAdapter(numbersArrayAdapter);
     }
 }
